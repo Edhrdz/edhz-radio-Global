5a0273c6dda4dd7105bd620ccb934a9884eee722
Edhrdz <cayfred2017@gmail.com>
2026-02-14 20:16:24 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,18 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     target: 'esnext',
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      // Indicamos a Rollup que estas librerías NO deben ser empaquetadas
+      // porque se resolverán en el navegador mediante el importmap
+      external: [
+        'react',
+        'react-dom',
+        'react-dom/client',
+        'framer-motion',
+        'lucide-react',
+        '@google/genai'
+      ]
+    }
   }
 });
